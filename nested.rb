c213b11ae67c1b6547604e8f33deefe08e606d9d
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-27 17:36:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     #programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
@@ -121,5 +121,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	#programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
